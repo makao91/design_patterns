e984a5caab4f67ff2a102eb9909eedfe782dcf1a
andrzej <a.fenzel@devpark.pl>
2021-09-27 15:59:52 +0200

    refactoring to decorator
    create abstract decorator and remove repeated code - DRY

--- Shipping/ShippingCostCalculator.php
+++ Shipping/ShippingCostCalculator.php
@@ -6,15 +6,9 @@ use App\Contracts\IPrice;
 use App\Contracts\IShippingOrder;
 use App\Contracts\IShippingClient;
 
-use App\Orders\Order;
 use App\Shipping\AdditionalCalculations\ClientShippingDiscount;
 use App\Shipping\AdditionalCalculations\FreeDeliveryDays;
 use App\Shipping\AdditionalCalculations\PremiumBox;
-use App\Shipping\CountryCalculators\CountryCalcFactory;
-
-use App\Shipping\Price\PricePl;
-use App\Shipping\Price\PriceUk;
-use App\Shipping\Price\PriceUs;
 use App\Shipping\Price\PriceFactory;
 
 class ShippingCostCalculator
@@ -26,9 +20,6 @@ class ShippingCostCalculator
     }
     public function calculate(ICountryShippingCalc $country_calc, IShippingClient $order):IPrice
     {
-        //all future additional calculations related with shipping only should start/be added here
-       // $shipping_cost = $country_calc->calculate($order);
-
         //include discounts for special premium days of delivery
         //IMPORTANT this has to be called before all other discounts calculations
         //e.g. Free delivery in special day is not equal that the premium box is free ;)
