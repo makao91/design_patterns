4168be0181ce9075a37e2adbb356f95d1bce8262
andrzej <a.fenzel@devpark.pl>
2021-09-27 10:50:10 +0200

    strategy pattern usage.
    version with factory inside of the refactored class
    
    force usage of interface

--- Shipping/ShippingCostCalculator.php
+++ Shipping/ShippingCostCalculator.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Shipping;
 
+use App\Contracts\ICountryShippingCalc;
 use App\Contracts\IPrice;
 use App\Orders\Order;
 use App\Shipping\CountryCalculators\CountryCalcFactory;
@@ -15,6 +16,18 @@ class ShippingCostCalculator
         $country = $order->getCountry();
 
         $country_calculator = (new CountryCalcFactory())->create($country);
-        return $country_calculator->calculate($order);
+        return $this->country_calc($country_calculator);
+    }
+
+    /**
+     * We use this method to force future programmers to use the proper interface
+     * implementation for calculation purphoses
+     * @param Order $order
+     * @param ICountryShippingCalc $calc
+     * @return IPrice
+     */
+    private function country_calc(Order $order, ICountryShippingCalc $calc):IPrice
+    {
+        return $calc->calculate($order);
     }
 }
