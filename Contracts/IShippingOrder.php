35505a67ef012e3ab69ee398d2aa8c420425e659
andrzej <a.fenzel@devpark.pl>
2021-09-29 06:47:56 +0200

    add missing method to shipping order interface

--- Contracts/IShippingOrder.php
+++ Contracts/IShippingOrder.php
@@ -8,4 +8,5 @@ interface IShippingOrder
     public function getTotalUk();
     public function getTotalUs();
     public function getShippingDiscount();
+    public function isPremiumBox():bool;
 }
